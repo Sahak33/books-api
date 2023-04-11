331dc1e8d92d914ce5ebb90a069a62bca8fd6b19
Sahak <s.iskanian33@gmail.com>
2023-04-11 16:30:08 +0400

    Unit test created for book search endpoint call, removed some old unnesesarry migrations edited .env.test for test database

---
+++ tests/api/book/BookTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BookTest extends WebTestCase
+{
+    public function testSearch()
+    {
+        $client = static::createClient();
+        $router = $client->getContainer()->get('router');
+
+        $url = $router->generate('api_book_search', ['id' => 1]);
+
+        $client->request('GET', $url);
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
