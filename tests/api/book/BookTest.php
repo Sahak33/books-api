8b6b253407a9390b92a3c0748f85e17eba96e8ec
Sahak <s.iskanian33@gmail.com>
2023-04-11 17:53:15 +0400

    BookSearch and search filters unit test

--- tests/api/book/BookTest.php
+++ tests/api/book/BookTest.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BookTest extends WebTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
     public function testSearch()
     {
         $client = static::createClient();
@@ -17,5 +21,86 @@ class BookTest extends WebTestCase
         $response = $client->getResponse();
 
         $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testSearchFilterPrice()
+    {
+        $client = static::createClient();
+        $router = $client->getContainer()->get('router');
+        $url = $router->generate('api_book_search');
+
+        $client->request('GET', $url);
+        $response = $client->getResponse();
+        $books = json_decode($response->getContent());
+        $sample_price = 0;
+        foreach ($books as $book){
+            $arrayData = json_decode(json_encode($book->published), true);
+            $item = $arrayData['price'] ?? null;
+            if($item){
+                $sample_price = $item;
+            }
+        }
+
+        $url = $router->generate('api_book_search', ['price' => $sample_price]);
+
+        $client->request('GET', $url);
+        $response = $client->getResponse();
+        $result = json_decode($response->getContent());
+
+        $this->assertTrue(count($result) > 0);
+    }
+
+    public function testSearchFilterDate()
+    {
+        $client = static::createClient();
+        $router = $client->getContainer()->get('router');
+        $url = $router->generate('api_book_search');
+
+        $client->request('GET', $url);
+        $response = $client->getResponse();
+        $books = json_decode($response->getContent());
+        $sample_price = 0;
+        foreach ($books as $book){
+            $arrayData = json_decode(json_encode($book->published), true);
+            $item = $arrayData['date'] ?? null;
+            if($item){
+                $sample_price = $item;
+            }
+        }
+
+        $url = $router->generate('api_book_search', ['date' => $sample_price]);
+
+        $client->request('GET', $url);
+        $response = $client->getResponse();
+        $result = json_decode($response->getContent());
+
+        $this->assertTrue(count($result) > 0);
+    }
+
+    public function testSearchFilterCategory()
+    {
+        $client = static::createClient();
+        $router = $client->getContainer()->get('router');
+        $url = $router->generate('api_book_search');
+
+        $client->request('GET', $url);
+        $response = $client->getResponse();
+        $books = json_decode($response->getContent());
+        $sample_price = 0;
+        foreach ($books as $book){
+            $arrayData = json_decode(json_encode($book->published), true);
+            $item = $arrayData['date'] ?? null;
+            if($item){
+                $sample_price = $item;
+            }
+        }
+
+        $url = $router->generate('api_book_search', ['date' => $sample_price]);
+
+        $client->request('GET', $url);
+        $response = $client->getResponse();
+        $result = json_decode($response->getContent());
+
+        $this->assertTrue(count($result) > 0);
     }
 }
